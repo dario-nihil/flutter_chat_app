1ece9056dcc3abdf25c0d3c3b16e00c6fba0e28c
dario-nihil <dario.nihil@gmail.com>
2024-03-25 08:52:13 +0100

    created NewMessage and ChatMessages widgets and started implemenetation

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChartMessages extends StatelessWidget {
+  const ChartMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('No messages found'),
+    );
+  }
+}
